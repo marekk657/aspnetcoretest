f798828aa9f9ca97949d520a00cecd0f84eb1b03
Marek Kikas <marek.kikas@agileworks.eu>
2016-11-04 12:37:59 +0200

    mongo test

--- src/aspnetcore/Controllers/ValuesController.cs
+++ src/aspnetcore/Controllers/ValuesController.cs
@@ -1,19 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace aspnetcore.Controllers
 {
+    public class TestData
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+    }
+
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<TestData> Get()
         {
-            return new string[] { "value1", "value2" };
+            var mongod = new MongoClient();
+            var db = mongod.GetDatabase("TestDatas");
+            var datas = db.GetCollection<TestData>("TestDataCollection");
+
+            var filter = new BsonDocument();
+            using (var cursor = datas.FindSync(filter))
+            {
+                while (cursor.MoveNext())
+                {
+                    var batch = cursor.Current;
+                    foreach (var document in batch)
+                    {
+                        yield return document;
+                    }
+                }
+            }
+
+        }
+
+        [HttpGet]
+        public void Get(ObjectId id, string name)
+        {
+            var mongod = new MongoClient();
+            var db = mongod.GetDatabase("TestDatas");
+            var datas = db.GetCollection<TestData>("TestDataCollection");
+
+            var testData = new TestData { Id = id, Name = name };
+            datas.InsertOne(testData);
         }
 
         // GET api/values/5
